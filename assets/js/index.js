2abbdca355a01354dbc4fbae8e4acec96c4c35de
Arsentii Tatarenko <tatarenkoarsen@gmail.com>
2021-09-22 22:24:53 +0300

    corrrections

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,42 @@
-const bgColors = ["#29BF12", "#ABFF4F", "#08BDBD", "#F21B3F", "#FF9914"];
-const body = document.body;
-const menu = body.querySelector(".menu");
-const menuItems = menu.querySelectorAll(".menu_item");
-const topBorder = menu.querySelector(".menu_top_border");
-let activItem = menu.querySelector(".active");
+const bgColors = ["#29BF12", "#ABFF4F", "#08BDBD", "#F21B3F", "#FF9914"],
+  body = document.body,
+  menu = body.querySelector(".menu"),
+  menuItems = menu.querySelectorAll(".menu_item"),
+  topBorder = menu.querySelector(".menu_top_border");
+let activItem = menu.querySelector(".active"),
+  declarationProperty = document.styleSheets[0].cssRules[0].style;
 
-body.style.setProperty("--bg-color", /*bgColors[0]*/ "#B4F8C8");
-// activItem.style.setProperty("--item-bg-colors", bgColors[0]);
-function itemClickFunction(item, index) {
-  if (activItem) activItem.classList.remove("active");
-  activItem = item;
-  body.style.backgroundColor = bgColors[index];
-  activItem.style.setProperty("--item-bg-colors", bgColors[index]);
-  item.classList.add("active");
-  moveBorder(activItem);
-}
+body.style.setProperty("--bg-color", "#B4F8C8");
 
-function moveBorder(activItem) {  
-
-  let move =
+function mooveBorder() {
+  let moove =
     activItem.getBoundingClientRect().left -
     menu.offsetLeft -
     topBorder.offsetWidth / 2 +
     activItem.offsetWidth / 2;
-  topBorder.style.setProperty("--translate-transform", `${move}px`);
+
+  return moove;
 }
 
-  if (!activItem) {
-    topBorder.style.setProperty(
-      "--translate-transform",
-      `0, ${topBorder.offsetHeight }px`
-    );
-  } 
+if (!activItem) {
+  topBorder.style.setProperty(
+    "--translate-transform",
+    `0, ${topBorder.offsetHeight}px`
+  );
+} else {
+  activItem.style.setProperty("--item-bg-colors", "#B4F8C8");
+  topBorder.style.setProperty("--translate-transform", `${mooveBorder()}px`);
+}
 
+function itemClickFunction(item, index) {
+  declarationProperty.setProperty("--transition-duration", "1s");
+  if (activItem) activItem.classList.remove("active");
+  activItem = item;
+  body.style.setProperty("--bg-color", `${bgColors[index]}`);
+  activItem.style.setProperty("--item-bg-colors", bgColors[index]);
+  item.classList.add("active");
+  topBorder.style.setProperty("--translate-transform", `${mooveBorder()}px`);
+}
 
 menuItems.forEach((item, index) =>
   item.addEventListener("click", () => itemClickFunction(item, index))
